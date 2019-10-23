54649c4c0d3121cc33e72746b2446fd07649f1a4
Mike Wolf <mw@mike-wolf.com>
2019-10-23 14:03:24 -0400

    Set up to use overmind

---
+++ src/app/index.js
@@ -0,0 +1,17 @@
+import { createOvermind } from "overmind";
+import { createHook } from "overmind-react";
+export let useApp;
+export let app;
+// app = createOvermind({
+//   state: {
+//     count: 0,
+//     logs: []
+//   },
+//   actions: {
+//     increaseCount({ state }, count = 1) {
+//       state.count += count;
+//     }
+//   }
+// });
+
+// useApp = createHook();
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Grandparent from "./components/Grandparent";
 import "./styles.css";
-
 function App() {
   return (
     <div className="App">
