d6c6ed087eb48c87d95ce30b9a08539feca24271
Mike Wolf <mw@mike-wolf.com>
2019-10-23 15:01:51 -0400

    Turned on overmind but not useApp

--- src/app/index.js
+++ src/app/index.js
@@ -2,16 +2,16 @@ import { createOvermind } from "overmind";
 import { createHook } from "overmind-react";
 export let useApp;
 export let app;
-// app = createOvermind({
-//   state: {
-//     count: 0,
-//     logs: []
-//   },
-//   actions: {
-//     increaseCount({ state }, count = 1) {
-//       state.count += count;
-//     }
-//   }
-// });
+app = createOvermind({
+  state: {
+    count: 0,
+    logs: []
+  },
+  actions: {
+    increaseCount({ state }, count = 1) {
+      state.count += count;
+    }
+  }
+});
 
-// useApp = createHook();
+useApp = createHook();
